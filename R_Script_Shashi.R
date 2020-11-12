10319a7eaa587540800b6d41e8ffe92e9fc7bdb3
Shashi Nagarajan Iyer <shashiniyer@gmail.com>
2020-11-12 23:27:51 +0100

    five

--- R_Script_Shashi.R
+++ R_Script_Shashi.R
@@ -2,11 +2,12 @@ library(readr)
 library(kknn)
 library(ggplot2)
 library(data.table)
+library(glmnet)
 
 ### Question 1
 
 # read in data
-optdigits <- readr::read_csv("optdigits.csv", col_names = FALSE)
+optdigits <- readr::read_csv("optdigits.csv", col_names = FALSE, col_types = cols())
 
 # treat Y variable "X65" as factor
 optdigits$X65 <- as.factor(optdigits$X65)
@@ -47,8 +48,8 @@ cat("Test mis-classification rate is: ", test_misclass, "%", sep = "")
 class_8_probs_for_true_8 <- model_30_NN_train$prob[train_data$X65 == 8, 9]
 class_8_indices <- which(train_data$X65 == 8)
 sorted_prob_indices <- order(class_8_probs_for_true_8)
-top_1_prob_data <- unlist(train_data[class_8_indices[sorted_prob_indices[length(sorted_indices)]], 1:64])
-top_2_prob_data <- unlist(train_data[class_8_indices[sorted_prob_indices[length(sorted_indices) - 1]], 1:64])
+top_1_prob_data <- unlist(train_data[class_8_indices[sorted_prob_indices[length(sorted_prob_indices)]], 1:64])
+top_2_prob_data <- unlist(train_data[class_8_indices[sorted_prob_indices[length(sorted_prob_indices) - 1]], 1:64])
 bott_1_prob_data <- unlist(train_data[class_8_indices[sorted_prob_indices[1]], 1:64])
 bott_2_prob_data <- unlist(train_data[class_8_indices[sorted_prob_indices[2]], 1:64])
 bott_3_prob_data <- unlist(train_data[class_8_indices[sorted_prob_indices[3]], 1:64])
@@ -76,9 +77,9 @@ num_classes <- length(unique(train_data$X65))
 num_train_examples <- nrow(train_data)
 num_val_examples <- nrow(val_data)
 one_hot_y_train <- t(sapply(as.numeric(train_data$X65), 
-                          function(x) {c(rep(0, x - 1), 1, rep(0, num_classes - x))}))
+                            function(x) {c(rep(0, x - 1), 1, rep(0, num_classes - x))}))
 one_hot_y_val <- t(sapply(as.numeric(val_data$X65), 
-                        function(x) {c(rep(0, x - 1), 1, rep(0, num_classes - x))}))
+                          function(x) {c(rep(0, x - 1), 1, rep(0, num_classes - x))}))
 
 for (k_choice in 1:30) {
   
@@ -119,16 +120,16 @@ ggplot(cross_entropy) + geom_line(aes(k, value, colour = Legend)) + theme_bw() +
 ### Question 2
 
 # read in data
-parkinsons <- read_csv("parkinsons.csv")
+parkinsons <- read_csv("parkinsons.csv", col_types = cols())
 
 # drop columns not to be used for prediction
 parkinsons <- parkinsons[, -c(1, 2, 3, 4, 6)]
 
 # bayesian model
 
-## $$\mathbf{y} | \mathbb{X}, w_0, \mathbf{w} \sim N(w_0 + \mathbf{w}\mathbb{X}, \sigma^2\cdot \mathbb{I})$$
-##  $$\mathbf{w} \sim N(0, \sigma^2/\lambda \cdot \mathbb{I})$$
-  
+## $$\mathbf{y} | \mathbb{X}, w_0, \mathbf{w} \sim N(w_0 + \mathbb{X}\mathbf{w}, \sigma^2\cdot \mathbb{I})$$
+## $$\mathbf{w} \sim N(0, \sigma^2/\lambda \cdot \mathbb{I})$$
+
 # create train/test partitions
 all_indices <- 1:(nrow(parkinsons))
 set.seed(12345)
@@ -171,7 +172,8 @@ ridge <- function(w_vector_sigma, X, lambda) { # built for no intercept models, 
 ridge_opt <- function(X, lambda) {
   
   n <- ncol(X)
-  par <- rnorm(n) # initial parameter values (to seed optimisation); random
+  par <- rnorm(n-1) # initial parameter values (to seed optimisation); random
+  par[n] <- 1 # initialise with positive value
   return(optim(par = par, fn = ridge, method = "BFGS", X = X, lambda = lambda)$par)
   
 }
@@ -210,9 +212,9 @@ model1_optimal_weights <- model1_optimal_weights_sigma[1:(n_col - 1)]
 model1_optimal_sigma <- model1_optimal_weights_sigma[n_col]
 model1_train_MSE <- MSE(train_data, model1_optimal_weights)
 model1_test_MSE <- MSE(test_data, model1_optimal_weights)
-model1_AIC <- -2*model_log_likelihood(model1_optimal_weights, model1_optimal_sigma, as.matrix(parkinsons)) + 
-  2*degrees_of_freedom(as.matrix(parkinsons), num_samples = 1000, 
-                       num_simulations = 50, model1_optimal_weights_sigma) # using full dataset rather than train / test, because why not!
+model1_AIC <- -2*model_log_likelihood(model1_optimal_weights, model1_optimal_sigma, as.matrix(train_data)) + 
+  2*degrees_of_freedom(as.matrix(train_data), num_samples = 1000, 
+                       num_simulations = 50, model1_optimal_weights_sigma)
 
 # model 2: lambda = 100
 model2_optimal_weights_sigma <- ridge_opt(train_data, lambda = 100)
@@ -220,9 +222,9 @@ model2_optimal_weights <- model2_optimal_weights_sigma[1:(n_col - 1)]
 model2_optimal_sigma <- model2_optimal_weights_sigma[n_col]
 model2_train_MSE <- MSE(train_data, model2_optimal_weights)
 model2_test_MSE <- MSE(test_data, model2_optimal_weights)
-model2_AIC <- -2*model_log_likelihood(model2_optimal_weights, model2_optimal_sigma, as.matrix(parkinsons)) + 
-  2*degrees_of_freedom(as.matrix(parkinsons), num_samples = 1000, 
-                       num_simulations = 50, model2_optimal_weights_sigma) # using full dataset rather than train / test, because why not!
+model2_AIC <- -2*model_log_likelihood(model2_optimal_weights, model2_optimal_sigma, as.matrix(train_data)) + 
+  2*degrees_of_freedom(as.matrix(train_data), num_samples = 1000, 
+                       num_simulations = 50, model2_optimal_weights_sigma)
 
 # model 3: lambda = 1000
 model3_optimal_weights_sigma <- ridge_opt(train_data, lambda = 1000)
@@ -230,9 +232,9 @@ model3_optimal_weights <- model3_optimal_weights_sigma[1:(n_col - 1)]
 model3_optimal_sigma <- model3_optimal_weights_sigma[n_col]
 model3_train_MSE <- MSE(train_data, model3_optimal_weights)
 model3_test_MSE <- MSE(test_data, model3_optimal_weights)
-model3_AIC <- -2*model_log_likelihood(model3_optimal_weights, model3_optimal_sigma, as.matrix(parkinsons)) + 
-  2*degrees_of_freedom(as.matrix(parkinsons), num_samples = 1000, 
-                       num_simulations = 50, model3_optimal_weights_sigma) # using full dataset rather than train / test, because why not!
+model3_AIC <- -2*model_log_likelihood(model3_optimal_weights, model3_optimal_sigma, as.matrix(train_data)) + 
+  2*degrees_of_freedom(as.matrix(train_data), num_samples = 1000, 
+                       num_simulations = 50, model3_optimal_weights_sigma)
 
 # plot performance
 MSE_data <- data.frame(log_10_lambda = c(0, 0, 2, 2, 3, 3), Legend = rep(c("Training", "Test"), 3), 
@@ -247,22 +249,22 @@ ggplot(MSE_data) + geom_line(aes(log_10_lambda, value, colour = Legend)) + theme
 AIC_data <- data.frame(log_10_lambda = c(0, 2, 3), value = c(model1_AIC, model2_AIC, model3_AIC))
 
 ggplot(AIC_data) + geom_line(aes(log_10_lambda, value)) + theme_bw() + 
-  geom_vline(xintercept = 3, linetype = "dotted") + 
+  geom_vline(xintercept = 2, linetype = "dotted") + 
   xlab("Log (base-10) of Hyper-Parameter 'lambda'") + ylab("Model AIC") + 
   ggtitle("Finding the Optimal Hyper-Parameter")
 
 ### Question 3
 
 # read in data
-tecator <- read_csv("tecator.csv")
+tecator <- read_csv("tecator.csv", col_types = cols())
 
 # drop columns not to be used for prediction
-tecator <- tecator[, -1]
+tecator <- tecator[, -c(1, 103, 104)]
 
 # split into training/test
 all_indices <- 1:(nrow(tecator))
 set.seed(12345)
-train_indices <- sample(all_indices, ceiling(0.6*nrow(tecator)))
+train_indices <- sample(all_indices, ceiling(0.5*nrow(tecator)))
 test_indices <- all_indices[!(all_indices %in% train_indices)]
 train_data <- tecator[train_indices, ]
 test_data <- tecator[test_indices, ]
@@ -273,6 +275,187 @@ test_data <- tecator[test_indices, ]
 
 # $$\mathbf{y} \sim N(\boldsymbol{\beta}\mathbb{X}, \sigma^2\cdot \mathbb{I})$$
 
-linreg <- lm(Y~., )
+# fit
+linreg <- lm(Fat~., train_data)
+y_train_pred <- predict(linreg)
+y_test_pred <- predict(linreg, test_data)
+train_mse <- sum((train_data$Fat - y_train_pred)^2)/nrow(train_data)
+test_mse <- sum((test_data$Fat - y_test_pred)^2)/nrow(test_data)
 
+cat("Training MSE is:", train_mse)
+cat("\nTest MSE is: ", test_mse)
 
+## lasso regression
+
+# objective function
+
+## $$\sum\limits_{i=1}^N{(y_i - \boldsymbol{\beta}\mathbb{X}_i)^2} + \lambda\sum\limits_{j=1}^p{|\beta_i|}$$
+
+# helper function: degrees of freedom
+lasso_degrees_of_freedom <- function(lasso_model, y, X, num_simulations, num_samples) {
+  
+  X <- as.matrix(X)
+  y <- unlist(y)
+  y_pred <- as.numeric(predict(lasso, X))
+  r <- nrow(X)
+  sum_cov = 0
+  
+  while (num_simulations > 0) {
+    
+    set.seed(num_simulations)
+    sample_indices <- sample(1:r, num_samples)
+    sample_X <- X[sample_indices, ]
+    sample_y <- y[sample_indices]
+    sample_y_pred <- y_pred[sample_indices]
+    sum_cov <- sum_cov + cov(sample_y, sample_y_pred)
+    num_simulations = num_simulations - 1
+    
+  }
+  
+  sigma_hat_square <- sum((y - y_pred)^2)/num_samples
+  
+  return(sum_cov/sigma_hat_square)
+  
+}
+
+# fit
+lambda_choices <- c(10^-3, 10^-2, 10^-1, 1, 10, 100, 1000)
+zero_coeff <- c()
+sum_abs_val_params <- c()
+lasso_deg_freedoms <- c()
+for (lam in lambda_choices){
+  
+  lasso <- glmnet(x = as.matrix(subset(train_data, select = -Fat)),
+                  y = unlist(train_data[, "Fat"]), alpha = 1, lambda = lam)
+  sum_abs_val_params <- c(sum_abs_val_params, sum(abs(lasso$beta)))
+  zero_coeff <- c(zero_coeff, sum(lasso$beta == 0))
+  lasso_deg_freedoms <- c(lasso_deg_freedoms, 
+                          lasso_degrees_of_freedom(
+                            lasso, tecator[, "Fat"], subset(tecator, select = -Fat), 
+                            num_simulations = 50, num_samples = 100)) # using full dataset rather than train / test, because why not!
+  
+}
+
+# plot results
+lasso_results <- data.frame(log_10_lambda = rep(-3:3, 2), 
+                            Legend = c(rep("# Parameters with Coefficients = 0", 7), 
+                                       rep("Sum of absolute values of Coefficients", 7)),
+                            value = c(zero_coeff, sum_abs_val_params))
+
+ggplot(lasso_results) + geom_line(aes(log_10_lambda, value, colour = Legend)) + theme_bw() + 
+  scale_x_continuous(breaks = -3:3) + theme(legend.position="bottom") + 
+  xlab("Log (base-10) of LASSO Hyper-Parameter 'lambda'") + ylab("Parameter Attributes") + 
+  ggtitle("Relationship between LASSO Lambda and parameter attributes")
+
+lasso_deg_freedom <- data.frame(log_10_lambda = rep(-3:3, 2), value = lasso_deg_freedoms)
+
+ggplot(lasso_deg_freedom) + geom_line(aes(log_10_lambda, value)) + theme_bw() +
+  scale_x_continuous(breaks = -3:3) + xlab("Log (base-10) of LASSO Hyper-Parameter 'lambda'") + 
+  ylab("Model Degrees of Freedom") + ggtitle("Relationship between LASSO Lambda and Model Degrees of Freedom")
+
+## ridge regression
+
+# fit
+zero_coeff <- c()
+sum_params_squared <- c()
+for (lam in lambda_choices){
+  
+  ridge <- glmnet(x = as.matrix(subset(train_data, select = -Fat)),
+                  y = unlist(train_data[, "Fat"]), alpha = 0, lambda = lam)
+  sum_params_squared <- c(sum_params_squared, sum(ridge$beta^2))
+  zero_coeff <- c(zero_coeff, sum(ridge$beta == 0))
+  
+}
+
+# plot results
+ridge_results <- data.frame(log_10_lambda = rep(-3:3, 2), 
+                            Legend = c(rep("# Parameters with Coefficients = 0", 7), 
+                                       rep("Sum of squared values of Coefficients", 7)),
+                            value = c(zero_coeff, sum_params_squared))
+
+ggplot(ridge_results) + geom_line(aes(log_10_lambda, value, colour = Legend)) + theme_bw() + 
+  scale_x_continuous(breaks = -3:3) + theme(legend.position="bottom") + 
+  xlab("Log (base-10) of Ridge Hyper-Parameter 'lambda'") + ylab("Parameter Attributes") + 
+  ggtitle("Relationship between Ridge Lambda and parameter attributes")
+
+## lasso with CV
+
+# fit
+cv_means <- c()
+cv_min_lambda <- c()
+for (i in 1:100) {
+  
+  cv_lasso <- cv.glmnet(x = as.matrix(subset(train_data, select = -Fat)), 
+                        y = unlist(train_data[, "Fat"]), alpha = 1, 
+                        lambda = lambda_choices, nfolds = 3)
+  cv_means <- cbind(cv_means, cv_lasso$cvm)
+  cv_min_lambda <- c(cv_min_lambda, cv_lasso$lambda.min)
+}
+
+# plot
+ggplot(data.frame(log_10_lambda = -3:3, cv_score = rev(rowMeans(cv_means)))) + 
+  geom_line(aes(log_10_lambda, cv_score)) + scale_x_continuous(breaks = -3:3) +
+  geom_vline(xintercept = -3, linetype = "dotted") + 
+  xlab("Log (base-10) of LASSO Hyper-Parameter 'lambda'") + ylab("Mean squared error across CV folds") + 
+  ggtitle("Relationship between LASSO Lambda and CV Scores")
+
+opt_lambda <- cv_lasso$lambda.min
+cat("Optimal Lambda is:", opt_lambda)
+
+model_optimal_lambda <- glmnet(x = as.matrix(subset(train_data, select = -Fat)),
+                               y = unlist(train_data[, "Fat"]), alpha = 1, lambda = opt_lambda)
+num_non_zero_params_opt_lambda <- sum(model_optimal_lambda$beta != 0)
+cat("Number of variables chosen in the model:", num_non_zero_params_opt_lambda)
+
+# comparing LASSO regression model with optimal lambda and another with log (base-10) lambda = -2
+model_log_lambda_minus_2 <- glmnet(x = as.matrix(subset(train_data, select = -Fat)),
+                                   y = unlist(train_data[, "Fat"]), alpha = 1, lambda = 10^-2)
+y_test <- unlist(test_data[, "Fat"])
+y_test_pred_opt_lambda <- predict(model_optimal_lambda, as.matrix(subset(test_data, select = -Fat)))
+y_test_log_lambda_minus_2 <- predict(model_log_lambda_minus_2, as.matrix(subset(test_data, select = -Fat)))
+
+num_non_zero_params_log_lambda_minus_2 <- sum(model_log_lambda_minus_2$beta != 0)
+
+opt_lambda_SSE <- sum((y_test - y_test_pred_opt_lambda)^2) 
+log_lambda_minus_2_SSE <- sum((y_test - y_test_log_lambda_minus_2)^2)
+
+opt_lambda_deg_freedom <- nrow(test_data) - num_non_zero_params_opt_lambda - 1
+log_lambda_minus_2_deg_freedom <- nrow(test_data) - num_non_zero_params_log_lambda_minus_2 - 1
+
+opt_lambda_MSE <- opt_lambda_SSE / opt_lambda_deg_freedom
+log_lambda_minus_2_MSE <- log_lambda_minus_2_SSE / log_lambda_minus_2_deg_freedom
+test_statistic <- opt_lambda_MSE / log_lambda_minus_2_MSE
+
+# Null Hypothesis: opt_lambda_MSE = log_lambda_minus_2_MSE
+# Alternative Hypothesis: opt_lambda_MSE < log_lambda_minus_2_MSE
+
+p_value <- pf(q = test_statistic, 
+              df1 = opt_lambda_deg_freedom, 
+              df2 = log_lambda_minus_2_deg_freedom)
+
+cat("The p-value for the test of null hypothesis that optimal lambda works similar to log(lambda) = -2",
+    "vs. the alternative that the former is better is:", p_value)
+cat("We, therefore, cannot conclude confidently that the optimal",
+    "lambda works significantly better than log(lambda) = -2")
+
+# plot predictions
+ggplot(data.frame(y_test, y_test_pred_opt_lambda)) + 
+  geom_point(aes(y_test, y_test_pred_opt_lambda)) + 
+  xlab("Test Labels") + ylab("Test Predictions") +
+  ggtitle("Goodness of fit - LASSO model with optimised lambda")
+
+## generative model
+
+# estimate sigma: use MLE now ;-)
+y_train <- unlist(train_data[, "Fat"])
+y_train_pred <- predict(model_optimal_lambda, as.matrix(subset(train_data, select = -Fat)))
+MLE_sigma_estimate <- sum((y_train - y_train_pred)^2)/nrow(train_data)
+
+# generate labels from distribution N(y_train_pred, MLE_sigma_estimate*I)
+y_generated <- rnorm(length(y_test), y_test_pred_opt_lambda, MLE_sigma_estimate)
+
+# plot generated labels vs. original labels
+ggplot(data.frame(y_test, y_generated)) + 
+  geom_point(aes(y_test, y_generated)) + 
+  xlab("Test Labels") + ylab("Generated Labels") +
+  ggtitle("Goodness of fit - Generative LASSO model with optimised lambda")
